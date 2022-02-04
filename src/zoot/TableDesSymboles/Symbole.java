a497cda72631f5464dfda0eef103e5176e4d8757
iopeti2u <iopeti2u@etu.univ-lorraine.fr>
2022-02-04 10:12:07 +0100

    Classe Symbole.

---
+++ src/zoot/TableDesSymboles/Symbole.java
@@ -0,0 +1,27 @@
+package zoot.TableDesSymboles;
+
+public class Symbole {
+    private String type;
+    private int deplacement;
+
+    public Symbole(String type, int depl) {
+        this.type = type;
+        this.deplacement = depl;
+    }
+    
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getDeplacement() {
+        return deplacement;
+    }
+
+    public void setDeplacement(int deplacement) {
+        this.deplacement = deplacement;
+    }
+}
