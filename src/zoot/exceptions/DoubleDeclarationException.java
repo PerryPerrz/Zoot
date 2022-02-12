e823438a7ea844c9de5a5d2f08a40eec9184957c
iopeti2u <iopeti2u@etu.univ-lorraine.fr>
2022-02-12 10:48:21 +0100

    Refonte exceptions.

--- src/zoot/exceptions/DoubleDeclarationException.java
+++ src/zoot/exceptions/DoubleDeclarationException.java
@@ -3,7 +3,7 @@ package zoot.exceptions;
 /**
  * Classe DoubleDeclarationException représentant l'exception se déclenchant lorsque le compilateur détecte une double déclaration d'une variable.
  */
-public class DoubleDeclarationException extends AnalyseSyntaxiqueException {
+public class DoubleDeclarationException extends AnalyseLexicaleSemantique {
     public DoubleDeclarationException(String m) {
         super(m);
     }
--- src/zoot/exceptions/VariableNonDeclareeException.java
+++ src/zoot/exceptions/VariableNonDeclareeException.java
@@ -3,7 +3,7 @@ package zoot.exceptions;
 /**
  * Classe VariableNonDeclareeException représentant l'exception se déclenchant lorsque le compilateur détecte une variable utilisée sans avoir été déclarée.
  */
-public class VariableNonDeclareeException extends AnalyseSyntaxiqueException {
+public class VariableNonDeclareeException extends AnalyseLexicaleSemantique {
     public VariableNonDeclareeException(String m) {
         super(m);
     }
