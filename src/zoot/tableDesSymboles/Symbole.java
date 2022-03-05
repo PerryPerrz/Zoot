6a07b4b706be76e793cf695c631fd29cc2d42c3f
iopeti2u <iopeti2u@etu.univ-lorraine.fr>
2022-03-05 09:35:33 +0100

    Revert du commit précedent.

--- src/zoot/tableDesSymboles/Symbole.java
+++ src/zoot/tableDesSymboles/Symbole.java
@@ -5,18 +5,15 @@ package zoot.tableDesSymboles;
  */
 public class Symbole {
     private String type;
-    private final int noBloc;
     private int deplacement;
-    private String etiquette;
 
     /**
      * Constructeur de la classe Symbole.
      *
      * @param type, type de la variable
      */
-    public Symbole(String type, int noBloc) {
+    public Symbole(String type) {
         this.type = type;
-        this.noBloc = noBloc;
     }
 
     /**
@@ -51,32 +48,5 @@ public class Symbole {
      */
     public void setDeplacement(int deplacement) {
         this.deplacement = deplacement;
-    }
-
-    /**
-     * Fonction qui retourne le numéro de bloc
-     *
-     * @return le numéro de bloc
-     */
-    public int getNoBloc() {
-        return noBloc;
-    }
-
-    /**
-     * Fonction qui retourne l'étiquette d'un bloc.
-     *
-     * @return l'étiquette d'un bloc
-     */
-    public String getEtiquette() {
-        return etiquette;
-    }
-
-    /**
-     * Procédure qui définit l'étiquette d'un bloc
-     *
-     * @param etiquette
-     */
-    public void setEtiquette(String etiquette) {
-        this.etiquette = etiquette;
     }
 }
